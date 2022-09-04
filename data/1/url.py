02abe106a561d325a12965c101f7e8b60069a9e7
lysanyikkseniia <kdlysanyuk@edu.hse>
2022-09-04 22:12:40 +0300

    datesets

---
+++ data/1/url.py
@@ -0,0 +1,2 @@
+for i in range(1957, 2015):
+    print("http://www.wdcb.ru/stp/data/geomagn.ind/dst/dst_wdc/dst", i, "_hv.txt", sep="")
---
+++ data/2/url.py
@@ -0,0 +1,2 @@
+for i in range(1957, 2015):
+    print("http://www.wdcb.ru/stp/data/geomagn.ind/dst/dst_iaga2002/Dst_", i, "-01-01_", i, "-12-31_D.txt", sep="")
